c7da0ba79bd3a9509fb38522440e98148cfe4961
Fernando <nandokferrari@gmail.com>
2022-12-04 08:32:59 -0300

    removed broken code and added graphql plugin

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -7,24 +7,21 @@
 const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::post.post", ({ strapi }) => ({
-  async find(ctx) {
-    const entity = await strapi.service("api::post.post").find(ctx);
-    const results = [...entity.results];
-    const shouldReplace = process.env.STRAPI_URL === "develop";
-    const absolutPathed = results.map((item) => {
-      let changedObject = {
-        ...item,
-        content: item.content.replace(
-          "/uploads",
-          `${process.env.STRAPI_URL}/uploads`
-        ),
-      };
-
-      return shouldReplace ? changedObject : item;
-    });
-
-    entity.results = absolutPathed;
-    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
-    return this.transformResponse(sanitizedEntity);
-  },
+  // async find(ctx) {
+  //   const entity = await strapi.service("api::post.post").find(ctx);
+  //   const results = [...entity.results];
+  //   const shouldReplace = process.env.STRAPI_URL === "develop";
+  //   const absolutPathed = results.map((item) => {
+  //     let changedObject = {
+  //       ...item,
+  //       content: item.content.replace(
+  //         "/uploads",
+  //         `${process.env.STRAPI_URL}/uploads`
+  //       ),
+  //     };
+  //     return shouldReplace ? changedObject : item;
+  //   });
+  //   const sanitizedEntity = await this.sanitizeOutput({ name: "asd" }, ctx);
+  //   return this.transformResponse(sanitizedEntity);
+  // },
 }));
